919d19bef5b3e39ab2fbd9cd9f97a8974e5c9437
subframe7536 <1667077010@qq.com>
2023-06-24 16:04:05 +0800

    docs: add jsdoc

--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,11 @@
 import { contextBridge } from 'electron'
 import type { IpcExposeName, SetupItem, TypesafeIpcRenderer } from './types'
 
+/**
+ * expose IPC to renderer
+ * @param modules predefined ipc modules
+ * @param option custom expose name
+ */
 export function exposeIPC(modules: TypesafeIpcRenderer<SetupItem>, option?: IpcExposeName) {
   const { channels, clearListeners, renderer } = modules
   contextBridge.exposeInMainWorld(option?.renderer ?? '__electron_renderer', renderer)
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,5 +1,9 @@
 import type { IpcExposeName, SetupItem, TypesafeIpcRenderer } from './types'
 
+/**
+ * load exposed IPC
+ * @param option custom expose name
+ */
 export function loadIPC<T extends SetupItem>(option?: IpcExposeName): TypesafeIpcRenderer<T> {
   return {
     renderer: option?.renderer ?? globalThis.__electron_renderer,
