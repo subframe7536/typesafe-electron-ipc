c06b0811b60e0f6d8538f6c0a27c0a42453f891a
subframe7536 <1667077010@qq.com>
2023-09-23 17:05:13 +0800

    chore: improve types and docs

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,12 +2,12 @@ import type { SetupItem, TypesafeIpcRenderer } from './types'
 
 /**
  * load exposed IPC
- * @param option custom expose name
+ * @param name custom expose name
  */
 export function loadIPC<T extends SetupItem>(name = '__electron_ipc'): TypesafeIpcRenderer<T> {
   return loadMain(name)
 }
 
-export function loadMain<T = any>(key: string): T {
-  return globalThis[key as string]
+export function loadMain<T>(key: string): T {
+  return globalThis[key]
 }
